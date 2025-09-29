0ccb4526bc311fbb8fa29c6ac2ea7497bbd65738
Joshua Linog <jsinkcreations@gmail.com>
2025-09-29 16:41:38 -0400

    add color changes dynamically to json and templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "./src/**/*.{njk,html,js,md}",
     "./src/_includes/**/*.{njk,html,js,md}",
   ],
+  safelist: [
+    "bg-blue-50",
+    "bg-pink-50",
+    "bg-green-50",
+    "text-blue-500",
+    "text-pink-500",
+    "text-green-500",
+  ],
   theme: {
     extend: {},
   },
